1c9f173f0c8c3c38f65a46dc822e390785daea43
Yen Chih Lin <yenchihlin@Yens-MacBook-Pro.local>
2020-10-20 12:20:45 -0700

    upload onlineShop

---
+++ src/main/java/onlineShop/dao/CustomerDaoImpl.java
@@ -0,0 +1,72 @@
+package onlineShop.dao;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import onlineShop.model.Authorities;
+import onlineShop.model.Cart;
+import onlineShop.model.Customer;
+import onlineShop.model.User;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void addCustomer(Customer customer) {
+		customer.getUser().setEnabled(true);
+		//user authorities
+		Authorities authorities = new Authorities();
+		authorities.setAuthorities("ROLE_USER");
+		authorities.setEmailId(customer.getUser().getEmailId());
+		//user's cart
+		Cart cart = new Cart();
+		cart.setCustomer(customer);
+		customer.setCart(cart);
+		//call session to get a session from sessionfactory
+		//use transaction through session to ensure all actions success or fail together
+		//this session is not http session, its' hiberate session to have transaction between database
+		//jdbc no session concept
+		Session session = null;
+
+		try {
+			session = sessionFactory.openSession();
+			session.beginTransaction();
+			session.save(authorities);
+			session.save(customer);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			//if any failure found, the success transaction will be rolled back
+			session.getTransaction().rollback();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	public Customer getCustomerByUserName(String userName) {
+		User user = null;
+		try (Session session = sessionFactory.openSession()) {
+			//hiberate 提供的方法, 去sql裡面找東西
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
+			Root<User> root = criteriaQuery.from(User.class);
+			criteriaQuery.select(root).where(builder.equal(root.get("emailId"), userName));
+			user = session.createQuery(criteriaQuery).getSingleResult();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (user != null)
+			return user.getCustomer();
+		return null;
+	}
+}
